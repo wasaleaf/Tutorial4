492f5be234b4e047b902e7dc79a76ef3315c938a
Vic Wassilieff <vicwassilieff123@gmail.com>
2020-08-19 12:36:35 +1200

    first commit for tut4

---
+++ src/test/java/tutorial4/TestCalc.java
@@ -0,0 +1,14 @@
+package tutorial4;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCalc {
+
+	@Test
+	public void testAdd() {
+		int x = 1;
+		int y = 2;
+		assertEquals(Calc.add(x, y), 3);
+	}
+}
